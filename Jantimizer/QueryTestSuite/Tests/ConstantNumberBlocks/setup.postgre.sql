8177227ba7593be8dba10c0c86fc6f4e234deb57
jamadaha <53914190+jamadaha@users.noreply.github.com>
2022-03-03 11:14:01 +0100

    Add analyze to setup

--- Jantimizer/QueryTestSuite/Tests/ConstantNumberBlocks/setup.postgre.sql
+++ Jantimizer/QueryTestSuite/Tests/ConstantNumberBlocks/setup.postgre.sql
@@ -2,3 +2,5 @@
 create table B as select s, (100)::integer from generate_Series(1,50) s;
 create table C as select s, (1000)::integer from generate_Series(1,100) s;
 create table D as select s, (10000)::integer from generate_Series(1,500) s;
+
+analyze;
--- Jantimizer/QueryTestSuite/Tests/RandomNumberBlocks/setup.postgre.sql
+++ Jantimizer/QueryTestSuite/Tests/RandomNumberBlocks/setup.postgre.sql
@@ -2,3 +2,5 @@ create table A as select s, (random() * 10)::integer from generate_Series(1,10) 
 create table B as select s, (random() * 100)::integer from generate_Series(1,50) s;
 create table C as select s, (random() * 1000)::integer from generate_Series(1,100) s;
 create table D as select s, (random() * 10000)::integer from generate_Series(1,500) s;
+
+analyze;
