c983d02c5a6ae6fe5bdc6c2198e43d918a5d7a2b
Kristian Skov Johansen <kris701kj@gmail.com>
2022-03-03 10:36:33 +0100

    * Made a service to take care of connection strings to the various database systems. * Added a readme in the `Services` folder on how to use it

---
+++ Jantimizer/QueryTestSuite/Services/SecretsService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryTestSuite.Services
+{
+    public class SecretsService
+    {
+        private IConfiguration configuration;
+
+        public SecretsService()
+        {
+            configuration = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+        }
+
+        public string GetConnectionString(string key)
+        {
+            return configuration.GetConnectionString(key);
+        }
+    }
+}
