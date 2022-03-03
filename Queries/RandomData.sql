f7793365c998a8ffb77b76e66ed34dfdaf608fcb
jamadaha <53914190+jamadaha@users.noreply.github.com>
2022-03-03 10:28:31 +0100

    Adds test cases for other join type

--- Queries/RandomData.sql
+++ Queries/RandomData.sql
@@ -1,3 +1,8 @@
+drop table if exists A;
+drop table if exists B;
+drop table if exists C;
+drop table if exists D;
+
 -- Create tables
 create table A as select s, (random() * 10)::integer from generate_Series(1,10) s;
 create table B as select s, (random() * 100)::integer from generate_Series(1,50) s;
@@ -23,6 +28,22 @@ explain analyze select * from (((a join b on a."int4" < b."int4") join c on b."i
 explain analyze select * from (((a join b on a."int4" < b."int4") join c on b."int4" > c."int4") join d on c."int4" > d."int4"); -- 927833 | 6800
 explain analyze select * from (((a join b on a."int4" > b."int4") join c on b."int4" > c."int4") join d on c."int4" < d."int4"); -- 927833 | 16000
 explain analyze select * from (((a join b on a."int4" > b."int4") join c on b."int4" > c."int4") join d on c."int4" > d."int4"); -- 927833 | 0
+
+explain analyze select * from a, b where a."int4" = b."int4";
+explain analyze select * from a, b where a."int4" < b."int4";
+explain analyze select * from a, b where a."int4" > b."int4";
+explain analyze select * from a, b where a."int4" <= b."int4";
+explain analyze select * from a, b where a."int4" >= b."int4";
+explain analyze select * from a, b, c where a."int4" = b."int4" and b."int4" = c."int4";
+explain analyze select * from a, b, c where a."int4" < b."int4" and b."int4" < c."int4";
+explain analyze select * from a, b, c where a."int4" > b."int4" and b."int4" > c."int4";
+explain analyze select * from a, b, c where a."int4" <= b."int4" and b."int4" <= c."int4";
+explain analyze select * from a, b, c where a."int4" >= b."int4" and b."int4" >= c."int4";
+explain analyze select * from a, b, c, d where a."int4" = b."int4" and b."int4" = c."int4" and c."int4" = d."int4";
+explain analyze select * from a, b, c, d where a."int4" < b."int4" and b."int4" < c."int4" and c."int4" < d."int4";
+explain analyze select * from a, b, c, d where a."int4" > b."int4" and b."int4" > c."int4" and c."int4" > d."int4";
+explain analyze select * from a, b, c, d where a."int4" <= b."int4" and b."int4" <= c."int4" and c."int4" <= d."int4";
+explain analyze select * from a, b, c, d where a."int4" >= b."int4" and b."int4" >= c."int4" and c."int4" >= d."int4";
 
 -- Clean up
 drop table if exists A;
