a6d5030312bc51596d43a1a1b6e77513fa68318c
Jan M. D. Hansen <53914190+jamadaha@users.noreply.github.com>
2022-03-10 15:43:52 +0100

    Adds more cost calculation

--- Jantimizer/QueryHandler/QueryHandler.cs
+++ Jantimizer/QueryHandler/QueryHandler.cs
@@ -79,12 +79,48 @@ namespace QueryHandler
             throw new NotImplementedException();
         }
         private int CalculateLessJoinCost(JoinNode joinNode, Histograms.Managers.PostgresEquiDepthHistogramManager histograms) {
-            throw new NotImplementedException();
+            IHistogram leftGram = histograms.GetHistogram(joinNode.LeftTable, joinNode.LeftAttribute);
+            IHistogram rightGram = histograms.GetHistogram(joinNode.RightTable, joinNode.RightAttribute);
+            int leftBucketStart = -1;
+            int leftBucketEnd = -1;
+            int rightBucketStart = -1;
+            int rightBucketEnd = -1;
+
+            for (int leftBucketIndex = 0; leftBucketIndex < leftGram.Buckets.Count; leftBucketIndex++)
+            {
+                for (int rightBucketIndex = 0; rightBucketIndex < rightGram.Buckets.Count; rightBucketIndex++)
+                {
+                    // If either rightBucketStart or end is in the range of the left bucket
+                    if (leftGram.Buckets[leftBucketIndex].ValueStart < rightGram.Buckets[rightBucketIndex].ValueEnd)
+                    {
+                        if (leftBucketStart == -1)
+                            leftBucketStart = leftBucketIndex;
+                        if (leftBucketEnd < leftBucketIndex)
+                            leftBucketEnd = leftBucketIndex;
+                        if (rightBucketStart == -1)
+                            rightBucketStart = rightBucketIndex;
+                        if (rightBucketEnd < rightBucketIndex)
+                            rightBucketEnd = rightBucketIndex;
+                        continue;
+                    }
+                }
+            }
+
+            // No overlap
+            if (leftBucketStart == -1 || leftBucketEnd == -1 || rightBucketEnd == -1 || rightBucketEnd == -1)
+                return 0;
+
+            int leftBucketCount = 0;
+            int rightBucketCount = 0;
+
+            for (int i = leftBucketStart; i <= leftBucketEnd; i++)
+                leftBucketCount += leftGram.Buckets[i].Count;
+            for (int i = rightBucketStart; i <= rightBucketEnd; i++)
+                rightBucketCount += rightGram.Buckets[i].Count;
+
+            return leftBucketCount * rightBucketCount;
         }
         private int CalculateMoreJoinCost(JoinNode joinNode, Histograms.Managers.PostgresEquiDepthHistogramManager histograms) {
-            throw new NotImplementedException();
-        }
-        private int CalculateNoneJoinCost(JoinNode joinNode, Histograms.Managers.PostgresEquiDepthHistogramManager histograms) {
             throw new NotImplementedException();
         }
     }
---
+++ Jantimizer/QueryHandlerTest/SingleJoin.cs
@@ -0,0 +1,133 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using QueryHandler;
+using Histograms;
+using Histograms.Managers;
+using QueryParser;
+using QueryParser.QueryParsers;
+
+
+
+namespace QueryHandlerTest;
+
+[TestClass]
+public class SingleJoin
+{
+    Histograms.HistogramEquiDepth CreateConstHistogram(string tableName, string attibuteName, int depth, int tableSize, int value) {
+        var tempGram = new Histograms.HistogramEquiDepth(tableName, attibuteName, depth);
+        List<int> values = new List<int>();
+        for (int i = 0; i < tableSize; i++)
+            values.Add(value);
+        tempGram.GenerateHistogram(values);
+        return tempGram;
+    }
+
+    Histograms.HistogramEquiDepth CreateIncreasingHistogram(string tableName, string attibuteName, int depth, int minValue, int maxValue) {
+        var tempGram = new Histograms.HistogramEquiDepth(tableName, attibuteName, depth);
+        List<int> values = new List<int>();
+        for (int i = minValue; i < maxValue; i++)
+            values.Add(i);
+        tempGram.GenerateHistogram(values);
+        return tempGram;
+    }
+
+    #region Equality
+
+    [TestMethod]
+    [DataRow(10, 100, 100, 10000, 10)]
+    [DataRow(20, 100, 100, 10000, 10)]
+    [DataRow(10, 10, 100, 1000, 10)]
+    [DataRow(10, 100, 100, 10000, 20)]
+    [DataRow(100, 1, 1, 1, 10)]
+    public void EqualitySameValue(int value, int aAmount, int bAmount, int expectedHits, int depth)
+    {
+        var histogramManager = new Histograms.Managers.PostgresEquiDepthHistogramManager("SomeConnectionString", depth);
+        var aGram = CreateConstHistogram("A", "ID", depth, aAmount, value);
+        var bGram = CreateConstHistogram("B", "ID", depth, bAmount, value);
+        histogramManager.AddHistogram(aGram);
+        histogramManager.AddHistogram(bGram);
+
+        QueryParser.ParserManager PM = new ParserManager(new List<IQueryParser>(){new JoinQueryParser()});
+        var nodes = PM.ParseQuery("SELECT * FROM (A JOIN B ON A.ID = B.ID) JOIN C ON B.ID = C.ID");
+
+        QueryHandler.QueryHandler QH = new QueryHandler.QueryHandler();
+        int cost = QH.CalculateJoinCost(nodes[0] as QueryParser.Models.JoinNode, histogramManager);
+
+        Assert.AreEqual(expectedHits, cost);
+    }
+    
+    [TestMethod]
+    [DataRow(0, 100, 0, 100, 10, 10000)]
+    [DataRow(0, 100, 99, 199, 10, 100)]
+    [DataRow(0, 100, 101, 201, 10, 0)]
+    [DataRow(0, 100, 50, 150, 10, 50*50)]
+    [DataRow(50, 150, 0, 100, 10, 50*50)]
+    [DataRow(0, 100, 50, 150, 20, 60*60)]
+    public void EqualityOverlap(int aMin, int aMax, int bMin, int bMax, int depth, int expectedHits) {
+        var histogramManager = new Histograms.Managers.PostgresEquiDepthHistogramManager("SomeConnectionString", depth);
+        var aGram = CreateIncreasingHistogram("A", "ID", depth, aMin, aMax);
+        var bGram = CreateIncreasingHistogram("B", "ID", depth, bMin, bMax);
+        histogramManager.AddHistogram(aGram);
+        histogramManager.AddHistogram(bGram);
+
+        QueryParser.ParserManager PM = new ParserManager(new List<IQueryParser>(){new JoinQueryParser()});
+        var nodes = PM.ParseQuery("SELECT * FROM (A JOIN B ON A.ID = B.ID) JOIN C ON B.ID = C.ID");
+
+        QueryHandler.QueryHandler QH = new QueryHandler.QueryHandler();
+        int cost = QH.CalculateJoinCost(nodes[0] as QueryParser.Models.JoinNode, histogramManager);
+
+        Assert.AreEqual(expectedHits, cost);
+    }
+    #endregion
+    #region Less
+    [TestMethod]
+    [DataRow(10, 20, 100, 100, 10, 100*100)]
+    [DataRow(20, 20, 100, 100, 10, 0)]
+    [DataRow(30, 20, 100, 100, 10, 0)]
+    [DataRow(10, 20, 50, 100, 10, 50*100)]
+    [DataRow(20, 20, 50, 100, 10, 0)]
+    [DataRow(30, 20, 50, 100, 10, 0)]
+    [DataRow(10, 20, 100, 50, 10, 50 * 100)]
+    [DataRow(20, 20, 100, 50, 10, 0)]
+    [DataRow(30, 20, 100, 50, 10, 0)]
+    [DataRow(10, 20, 100, 100, 20, 100 * 100)]
+    [DataRow(20, 20, 100, 100, 20, 0)]
+    [DataRow(30, 20, 100, 100, 20, 0)]
+    public void LessConstantValue(int aValue, int bValue, int aAmount, int bAmount, int depth, int expectedHits)
+    {
+        var histogramManager = new Histograms.Managers.PostgresEquiDepthHistogramManager("SomeConnectionString", depth);
+        var aGram = CreateConstHistogram("A", "ID", depth, aAmount, aValue);
+        var bGram = CreateConstHistogram("B", "ID", depth, bAmount, bValue);
+        histogramManager.AddHistogram(aGram);
+        histogramManager.AddHistogram(bGram);
+
+        QueryParser.ParserManager PM = new ParserManager(new List<IQueryParser>() { new JoinQueryParser() });
+        var nodes = PM.ParseQuery("SELECT * FROM (A JOIN B ON A.ID < B.ID) JOIN C ON B.ID < C.ID");
+
+        QueryHandler.QueryHandler QH = new QueryHandler.QueryHandler();
+        int cost = QH.CalculateJoinCost(nodes[0] as QueryParser.Models.JoinNode, histogramManager);
+
+        Assert.AreEqual(expectedHits, cost);
+    }
+    [TestMethod]
+    [DataRow(0, 100, 0, 100, 10, 100*100)]
+    [DataRow(0, 100, 100, 200, 10, 100*100)]
+    [DataRow(100, 200, 0, 100, 10, 0)]
+    public void LessIncreasingValue(int aMin, int aMax, int bMin, int bMax, int depth, int expectedHits)
+    {
+        var histogramManager = new Histograms.Managers.PostgresEquiDepthHistogramManager("SomeConnectionString", depth);
+        var aGram = CreateIncreasingHistogram("A", "ID", depth, aMin, aMax);
+        var bGram = CreateIncreasingHistogram("B", "ID", depth, bMin, bMax);
+        histogramManager.AddHistogram(aGram);
+        histogramManager.AddHistogram(bGram);
+
+        QueryParser.ParserManager PM = new ParserManager(new List<IQueryParser>() { new JoinQueryParser() });
+        var nodes = PM.ParseQuery("SELECT * FROM (A JOIN B ON A.ID < B.ID) JOIN C ON B.ID < C.ID");
+
+        QueryHandler.QueryHandler QH = new QueryHandler.QueryHandler();
+        int cost = QH.CalculateJoinCost(nodes[0] as QueryParser.Models.JoinNode, histogramManager);
+
+        Assert.AreEqual(expectedHits, cost);
+    }
+    #endregion
+}
