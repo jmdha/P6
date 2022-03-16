41d749b91b7a9871b3df823bb4d2266fc49d4587
Kristian Skov Johansen <kris701kj@gmail.com>
2022-03-16 13:49:45 +0100

    Added initial tests for `HistogramEquiDepth` and `HistogramBucket`

--- Jantimizer/Histograms/HistogramModels/HistogramBucket.cs
+++ Jantimizer/Histograms/HistogramModels/HistogramBucket.cs
@@ -3,8 +3,18 @@
     public class HistogramBucket : IHistogramBucket
     {
         public int ValueStart { get; }
-        public int ValueEnd { get; set; }
-        public int Count { get; set; }
+        private int _valueEnd;
+        public int ValueEnd { get => _valueEnd; set {
+                if (value < ValueStart)
+                    throw new IndexOutOfRangeException("Bucket end value cannot be lower than start value!");
+                _valueEnd = value;
+            } }
+        private int count;
+        public int Count { get => count; set { 
+                if (value < 0)
+                    throw new IndexOutOfRangeException("Count for a bucket cannot be less than 0!");
+                count = value;
+            } }
 
         public HistogramBucket(int valueStart, int valueEnd, int count)
         {
