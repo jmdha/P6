0f7b032b24ee3e9c2ec2d4d99e75a36e29be7494
Jan M. D. Hansen <53914190+jamadaha@users.noreply.github.com>
2022-03-16 16:07:05 +0100

    Adds query optimiser test

--- Jantimizer/QueryOptimiserTest/QueryOptimiserTest.cs
+++ Jantimizer/QueryOptimiserTest/QueryOptimiserTest.cs
@@ -31,6 +31,21 @@ public class QueryOptimiserTest
     )]
     [DataRow(
     1,                                                                                                  // testID
+    new int[] { 0, 1 },                                                                                 // expectedNodeOrder
+    new string[] { "A", "B", "C" },                                                                     // name
+    new int[] { 10, 10, 10 },                                                                           // depth
+    new int[] { 0, 0, 0 },                                                                              // min
+    new int[] { 50, 100, 150 },                                                                        // max
+    new int[] { 0, 1 },                                                                                 // nodeId
+    new JoinNode.ComparisonType[] { JoinNode.ComparisonType.Equal, JoinNode.ComparisonType.Equal },     // nodeComparisonType
+    new string[] { "A", "B" },                                                                          // nodeLeftTable
+    new string[] { "B", "C" },                                                                          // nodeRightTable
+    new string[] { "ID", "ID" },                                                                        // nodeLeftAttribute
+    new string[] { "ID", "ID" },                                                                        // nodeRightAttribute
+    new string[] { "A < B", "B < C" }                                                                   // nodeCondition
+    )]
+    [DataRow(
+    2,                                                                                                  // testID
     new int[] { 1, 0 },                                                                                 // expectedNodeOrder
     new string[] { "A", "B", "C" },                                                                     // name
     new int[] { 10, 10, 10 },                                                                           // depth
@@ -43,6 +58,36 @@ public class QueryOptimiserTest
     new string[] { "ID", "ID" },                                                                        // nodeLeftAttribute
     new string[] { "ID", "ID" },                                                                        // nodeRightAttribute
     new string[] { "A = B", "B = C" }                                                                   // nodeCondition
+    )]
+    [DataRow(
+    3,                                                                                                  // testID
+    new int[] { 0, 1 },                                                                                 // expectedNodeOrder
+    new string[] { "A", "B", "C" },                                                                     // name
+    new int[] { 10, 10, 10 },                                                                           // depth
+    new int[] { 0, 0, 0 },                                                                              // min
+    new int[] { 150, 100, 50 },                                                                         // max
+    new int[] { 1, 0 },                                                                                 // nodeId
+    new JoinNode.ComparisonType[] { JoinNode.ComparisonType.More, JoinNode.ComparisonType.More },       // nodeComparisonType
+    new string[] { "A", "B" },                                                                          // nodeLeftTable
+    new string[] { "B", "C" },                                                                          // nodeRightTable
+    new string[] { "ID", "ID" },                                                                        // nodeLeftAttribute
+    new string[] { "ID", "ID" },                                                                        // nodeRightAttribute
+    new string[] { "A > B", "B > C" }                                                                   // nodeCondition
+    )]
+    [DataRow(
+    4,                                                                                                  // testID
+    new int[] { 0, 1 },                                                                                 // expectedNodeOrder
+    new string[] { "A", "B", "C" },                                                                     // name
+    new int[] { 10, 10, 10 },                                                                           // depth
+    new int[] { 0, 0, 0 },                                                                              // min
+    new int[] { 50, 100, 150 },                                                                         // max
+    new int[] { 1, 0 },                                                                                 // nodeId
+    new JoinNode.ComparisonType[] { JoinNode.ComparisonType.Less, JoinNode.ComparisonType.Less },       // nodeComparisonType
+    new string[] { "B", "A" },                                                                          // nodeLeftTable
+    new string[] { "C", "B" },                                                                          // nodeRightTable
+    new string[] { "ID", "ID" },                                                                        // nodeLeftAttribute
+    new string[] { "ID", "ID" },                                                                        // nodeRightAttribute
+    new string[] { "B < C", "A < B" }                                                                   // nodeCondition
     )]
 
     public void OptimiseJoinQueryEqual(int testID, int[] expectedNodeOrder, string[] name, int[] depth, int[] min, int[] max, int[] nodeId, JoinNode.ComparisonType[] nodeComparisonType, string[] nodeLeftTable, string[] nodeRightTable, string[] nodeLeftAttribute, string[] nodeRightAttribute, string[] nodeCondition)
