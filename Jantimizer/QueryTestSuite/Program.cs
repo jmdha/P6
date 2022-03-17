00c913b33acaaaedda9166cad29350409f90dd22
Kristian Skov Johansen <kris701kj@gmail.com>
2022-03-17 09:23:39 +0100

    Moved the setup data for the `SuiteData` for MySQL and Postgre out into two static files instead

--- Jantimizer/QueryTestSuite/Program.cs
+++ Jantimizer/QueryTestSuite/Program.cs
@@ -10,6 +10,7 @@ using QueryParser.QueryParsers;
 using QueryPlanParser.Parsers;
 using QueryTestSuite.Models;
 using QueryTestSuite.Services;
+using QueryTestSuite.SuiteDatas;
 using QueryTestSuite.TestRunners;
 using Tools.Models;
 using Tools.Services;
@@ -27,55 +28,9 @@ namespace QueryTestSuite
         {
             SecretsService<Program> secrets = new SecretsService<Program>();
 
-            string postConnectionString = secrets.GetConnectionString("POSGRESQL");
-            var postConnectionProperties = new ConnectionProperties(
-                postConnectionString,
-                secrets.GetConnectionStringValue(postConnectionString, "Host"),
-                int.Parse(secrets.GetConnectionStringValue(postConnectionString, "Port")),
-                secrets.GetConnectionStringValue(postConnectionString, "Username"),
-                secrets.GetConnectionStringValue(postConnectionString, "Password"),
-                secrets.GetConnectionStringValue(postConnectionString, "Database"),
-                secrets.GetConnectionStringValue(postConnectionString, "SearchPath"));
-            var postConnector = new PostgreSqlConnector(postConnectionProperties);
-            var postPlanParser = new PostgreSqlParser();
-            var postHistoManager = new PostgresEquiDepthHistogramManager(postConnector.ConnectionProperties, 10);
-            var postOptimiser = new QueryOptimiserEquiDepth(postHistoManager);
-            var postParserManager = new ParserManager(new List<IQueryParser>() { new PostgresParser(postConnector) });
-            var postGenerator = new PostgresGenerator();
-            var postgresModel = new SuiteData(
-                "postgre",
-                postConnector,
-                postPlanParser,
-                postHistoManager,
-                postOptimiser,
-                postParserManager,
-                postGenerator);
-
-            string mySQLConnectionString = secrets.GetConnectionString("MYSQL");
-            var mySQLConnectionProperties = new ConnectionProperties(
-                mySQLConnectionString,
-                secrets.GetConnectionStringValue(mySQLConnectionString, "Server"),
-                int.Parse(secrets.GetConnectionStringValue(mySQLConnectionString, "Port")),
-                secrets.GetConnectionStringValue(mySQLConnectionString, "Uid"),
-                secrets.GetConnectionStringValue(mySQLConnectionString, "Pwd"),
-                secrets.GetConnectionStringValue(mySQLConnectionString, "Database"),
-                secrets.GetConnectionStringValue(mySQLConnectionString, "Database"));
-            var mySQLConnector = new DatabaseConnector.Connectors.MySqlConnector(mySQLConnectionProperties);
-            var mySQLPlanParser = new MySQLParser();
-            var mySQLHistoManager = new MySQLEquiDepthHistogramManager(mySQLConnector.ConnectionProperties, 10);
-            var mySQLOptimiser = new QueryOptimiserEquiDepth(mySQLHistoManager);
-            var mySQLParserManager = new ParserManager(new List<IQueryParser>() { new JoinQueryParser() });
-            var mySQLGenerator = new MySQLGenerator();
-            var mySQLModel = new SuiteData(
-                "mysql",
-                mySQLConnector,
-                mySQLPlanParser,
-                mySQLHistoManager,
-                mySQLOptimiser,
-                mySQLParserManager,
-                mySQLGenerator);
-
-            var connectorSet = new List<SuiteData>() { mySQLModel, postgresModel };
+            var connectorSet = new List<SuiteData>() { 
+                MySQLEquiDepthData.GetData(secrets),
+                PostgreEquiDepthData.GetData(secrets)};
 
             if (await DatabaseStarter.CheckAndStartServers(connectorSet))
             {
