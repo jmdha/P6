d14d45b6842087567eeb7b8940e7c373913d102e
Henrik Ginnerup <hginnerup@hotmail.com>
2022-03-29 10:18:50 +0200

    Added helper for regex matching

---
+++ Jantimizer/Tools/Regex/RegexHelperFunctions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Tools.Regex
+{
+    public static class RegexHelperFunctions
+    {
+
+        public static T GetRegexVal<T>(Match regexMatch, string name)
+        {
+
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentNullException(nameof(name));
+
+            Type nonNullableT = ToNotNullable(typeof(T));
+
+            if (!regexMatch.Success)
+                throw new ArgumentException("Regex wasn't matched");
+
+            if (!regexMatch.Groups[name].Success)
+            {
+                if (typeof(T) != nonNullableT) // If T is nullable
+                    return default(T)!; // I assume it's null, but compiler wont accept 'null'
+                else
+                    throw new ArgumentException($"Regex not matched for group '{name}'");
+            }
+
+
+            return (T)Convert.ChangeType(regexMatch.Groups[name].Value, nonNullableT, CultureInfo.InvariantCulture);
+        }
+
+        private static Type ToNotNullable(Type T)
+        {
+            // Underlying is null if T was non-nullable
+            Type? underlying = Nullable.GetUnderlyingType(T);
+            if (underlying != null)
+                return underlying;
+
+            return T;
+        }
+    }
+}
---
+++ Jantimizer/ToolsTests/RegexTests.cs
@@ -0,0 +1,70 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text.RegularExpressions;
+using Tools.Regex;
+
+namespace ToolsTests
+{
+    [TestClass]
+    public class RegexTests
+    {
+        #region GetRegexVal
+
+        [TestMethod]
+        [DataRow("start65end", 65)]
+        public void Can_GetRegexVal_Int(string str, int expected)
+        {
+            // Arrange
+            Regex regex = new Regex("start(?<groupName>.+)end$");
+
+            // Act
+            var match = regex.Match(str);
+
+            int actual = RegexHelperFunctions.GetRegexVal<int>(match, "groupName");
+
+            // Assert
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOfType(actual, typeof(int));
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow("startJanend", "Jan")]
+        [DataRow("start end", " ")]
+        public void Can_GetRegexVal_String(string str, string expected)
+        {
+            // Arrange
+            Regex regex = new Regex("start(?<groupName>.+)end$");
+
+            // Act
+            var match = regex.Match(str);
+
+            string actual = RegexHelperFunctions.GetRegexVal<string>(match, "groupName");
+
+            // Assert
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOfType(actual, typeof(string));
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow("startend")]
+        public void Can_GetRegexVal_Nullable(string str)
+        {
+            // Arrange
+            Regex regex = new Regex("start(?<groupName>.+)?end$");
+
+            // Act
+            var match = regex.Match(str);
+
+            int? actual = RegexHelperFunctions.GetRegexVal<int?>(match, "groupName");
+
+            // Assert
+            Assert.IsNull(actual);
+        }
+
+        #endregion
+
+
+    }
+}
