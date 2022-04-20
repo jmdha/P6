fa5919dae0fdb199387dd0d07d5a4f65b5251e08
Kristian Skov Johansen <kris701kj@gmail.com>
2022-04-20 09:39:10 +0200

    Removed some ref types

--- Jantimizer/QueryOptimiser/Cost/EstimateCalculators/BaseEstimateCalculator.cs
+++ Jantimizer/QueryOptimiser/Cost/EstimateCalculators/BaseEstimateCalculator.cs
@@ -36,39 +36,33 @@ namespace QueryOptimiser.Cost.EstimateCalculators
             if (node.Relation == null)
                 throw new ArgumentException("node relation is not allowed to be null " + node.ToString());
 
-            List<IntermediateBucket> buckets;
-            List<TableAttribute> references = new List<TableAttribute>();
-
-            buckets = GetBucketMatches(node.Relation, table, ref references);
-
-            return new IntermediateTable(buckets, references);
+            BucketMatches bucketMatches = GetBucketMatches(node.Relation, table);
+            return new IntermediateTable(bucketMatches.Buckets, bucketMatches.References);
         }
 
         #region Matches
 
-        internal List<IntermediateBucket> GetBucketMatches(JoinPredicateRelation relation, IntermediateTable table, ref List<TableAttribute> references)
+        internal BucketMatches GetBucketMatches(JoinPredicateRelation relation, IntermediateTable table)
         {
             if (relation.Type == RelationType.Type.Predicate && relation.LeafPredicate != null)
-                return GetBucketMatches(relation.LeafPredicate, table, ref references);
+                return GetBucketMatches(relation.LeafPredicate, table);
             else if (relation.LeftRelation != null && relation.RightRelation != null)
             {
-                List<TableAttribute> leftReferences = new List<TableAttribute>();
-                List<TableAttribute> rightReferences = new List<TableAttribute>();
-                List<IntermediateBucket> leftBuckets = GetBucketMatches(relation.LeftRelation, table, ref leftReferences);
-                List<IntermediateBucket> rightBuckets = GetBucketMatches(relation.RightRelation, table, ref rightReferences);
+                BucketMatches leftBuckets = GetBucketMatches(relation.LeftRelation, table);
+                BucketMatches rightBuckets = GetBucketMatches(relation.RightRelation, table);
 
-                List<IntermediateBucket> overlap = new List<IntermediateBucket>();
+                BucketMatches overlap = new BucketMatches();
                 if (relation.Type == RelationType.Type.And)
                 {
-                    overlap = BucketHelper.MergeOnOverlap(leftBuckets, rightBuckets);
-                    references = GetOverlappingReferences(leftReferences, rightReferences);
+                    overlap.Buckets = BucketHelper.MergeOnOverlap(leftBuckets.Buckets, rightBuckets.Buckets);
+                    overlap.References = GetOverlappingReferences(leftBuckets.References, rightBuckets.References);
                 }
                 else if (relation.Type == RelationType.Type.Or)
                 {
-                    overlap.AddRange(leftBuckets);
-                    overlap.AddRange(rightBuckets);
-                    references.AddRange(leftReferences);
-                    references.AddRange(rightReferences);
+                    overlap.Buckets.AddRange(leftBuckets.Buckets);
+                    overlap.Buckets.AddRange(rightBuckets.Buckets);
+                    overlap.References.AddRange(leftBuckets.References);
+                    overlap.References.AddRange(rightBuckets.References);
                 }
                 
                 return overlap;
@@ -77,8 +71,9 @@ namespace QueryOptimiser.Cost.EstimateCalculators
                 throw new ArgumentException("Missing noderelation type " + relation.ToString());
         }
 
-        internal List<IntermediateBucket> GetBucketMatches(JoinPredicate predicate, IntermediateTable table, ref List<TableAttribute> references)
+        internal BucketMatches GetBucketMatches(JoinPredicate predicate, IntermediateTable table)
         {
+            BucketMatches returnMatches = new BucketMatches();
             PairBucketList bucketPair = GetBucketPair(
                 new TableAttribute(predicate.LeftTable.TableName, predicate.LeftAttribute),
                 new TableAttribute(predicate.RightTable.TableName, predicate.RightAttribute),
@@ -86,15 +81,16 @@ namespace QueryOptimiser.Cost.EstimateCalculators
             PairBucketList bounds = GetBucketBounds(predicate.ComType, bucketPair.LeftBuckets, bucketPair.RightBuckets);
 
             if (bounds.LeftBuckets.Count > 0 && bounds.RightBuckets.Count > 0) {
-                references.Add(new TableAttribute(predicate.LeftTable.TableName, predicate.LeftAttribute));
-                references.Add(new TableAttribute(predicate.RightTable.TableName, predicate.RightAttribute));
+                returnMatches.References.Add(new TableAttribute(predicate.LeftTable.TableName, predicate.LeftAttribute));
+                returnMatches.References.Add(new TableAttribute(predicate.RightTable.TableName, predicate.RightAttribute));
             } else
-                return new List<IntermediateBucket>();
+                return returnMatches;
 
             if (predicate.ComType == ComparisonType.Type.Equal)
-                return GetEqualityMatches(predicate, bounds.LeftBuckets, bounds.RightBuckets);
+                returnMatches.Buckets = GetEqualityMatches(predicate, bounds.LeftBuckets, bounds.RightBuckets);
             else
-                return GetInEqualityMatches(predicate, bounds.LeftBuckets, bounds.RightBuckets);
+                returnMatches.Buckets = GetInEqualityMatches(predicate, bounds.LeftBuckets, bounds.RightBuckets);
+            return returnMatches;
         }
 
         internal List<IntermediateBucket> GetEqualityMatches(JoinPredicate predicate, List<IHistogramBucket> leftBuckets, List<IHistogramBucket> rightBuckets)
@@ -196,7 +192,9 @@ namespace QueryOptimiser.Cost.EstimateCalculators
         }
 
         #endregion
+
         #region Bounds
+
         internal PairBucketList GetBucketBounds(ComparisonType.Type predicateType, List<IHistogramBucket> leftBuckets, List<IHistogramBucket> rightBuckets)
         {
             int leftStart = 0;
@@ -204,23 +202,23 @@ namespace QueryOptimiser.Cost.EstimateCalculators
             int rightStart = 0;
             int rightEnd = rightBuckets.Count - 1;
 
-            if (leftBuckets[0].ValueStart.CompareTo(rightBuckets[0].ValueStart) < 0)
+            if (leftBuckets[0].ValueStart.IsLessThan(rightBuckets[0].ValueStart))
             {
                 if (predicateType != ComparisonType.Type.Less && predicateType != ComparisonType.Type.EqualOrLess)
                     leftStart = GetMatchBucketIndex(leftBuckets, 0, leftBuckets.Count - 1, rightBuckets[0].ValueStart);
             }
-            else if (leftBuckets[0].ValueStart.CompareTo(rightBuckets[0].ValueStart) > 0)
+            else if (leftBuckets[0].ValueStart.IsLargerThan(rightBuckets[0].ValueStart))
             {
                 if (predicateType != ComparisonType.Type.More && predicateType != ComparisonType.Type.EqualOrMore)
                     rightStart = GetMatchBucketIndex(rightBuckets, 0, rightBuckets.Count - 1, leftBuckets[0].ValueStart);
             }
 
-            if (leftBuckets[leftBuckets.Count - 1].ValueEnd.CompareTo(rightBuckets[rightBuckets.Count - 1].ValueEnd) > 0)
+            if (leftBuckets[leftBuckets.Count - 1].ValueEnd.IsLargerThan(rightBuckets[rightBuckets.Count - 1].ValueEnd))
             {
                 if (predicateType != ComparisonType.Type.More && predicateType != ComparisonType.Type.EqualOrMore)
                     leftEnd = GetMatchBucketIndex(leftBuckets, 0, leftBuckets.Count - 1, rightBuckets[rightBuckets.Count - 1].ValueEnd);
             }
-            else if (leftBuckets[leftBuckets.Count - 1].ValueEnd.CompareTo(rightBuckets[rightBuckets.Count - 1].ValueEnd) < 0)
+            else if (leftBuckets[leftBuckets.Count - 1].ValueEnd.IsLessThan(rightBuckets[rightBuckets.Count - 1].ValueEnd))
             {
                 if (predicateType != ComparisonType.Type.Less && predicateType != ComparisonType.Type.EqualOrLess)
                     rightEnd = GetMatchBucketIndex(rightBuckets, 0, rightBuckets.Count - 1, leftBuckets[leftBuckets.Count - 1].ValueEnd);
@@ -255,6 +253,7 @@ namespace QueryOptimiser.Cost.EstimateCalculators
         }
 
         #endregion
+
         internal PairBucketList GetBucketPair(TableAttribute leftTableAttribute, TableAttribute rightTableAttribute, IntermediateTable table)
         {
             if (table.References.Contains(leftTableAttribute))
