6cd20fd5fcedd6f19f714b11a52514cb37fbc750
Kristian Skov Johansen <kris701kj@gmail.com>
2022-04-27 20:20:21 +0200

    Replaced all old queries in the basic tests with the new ones

--- Jantimizer/ExperimentSuite/Tests/BasicTests_ClumpedNumberBlocks/Cases/NJ_BE3.sql
+++
@@ -1,1 +0,0 @@
-SELECT * FROM (a JOIN b ON b.v >= a.v) JOIN c ON c.v >= b.v
--- Jantimizer/ExperimentSuite/Tests/BasicTests_ClumpedNumberBlocks/Cases/NJ_Or4.sql
+++
@@ -1,1 +0,0 @@
-﻿SELECT * FROM ((a JOIN b ON (a.v = b.v OR a.v < b.v)) JOIN c ON (b.v = c.v OR b.v < c.v)) JOIN d ON (c.v = d.v OR c.v < d.v)
