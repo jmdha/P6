51402af954c0d23f3810578018d4ab2b5a80daea
Kristian Skov Johansen <kris701kj@gmail.com>
2022-04-27 20:36:15 +0200

    Replaced remaining queries

--- Jantimizer/ExperimentSuite/Tests/WorldHappiness_With_GDP/Cases/country_codes_less_happy_than_denmark.sql
+++
@@ -1,6 +0,0 @@
-﻿SELECT C.code FROM 
-	(world_happiness_2022 AS H1 JOIN world_happiness_2022 AS H2 ON 
-	 	H1.country = 'Denmark' AND 
-	 	H1.happiness_score > H2.happiness_score)
-	JOIN
-	countries AS C ON C.name = H2.country;
