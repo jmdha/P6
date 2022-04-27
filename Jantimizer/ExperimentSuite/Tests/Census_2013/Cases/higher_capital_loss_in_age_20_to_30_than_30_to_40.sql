5b19c94507f2e321b1db003af34987d1ab11bf72
Kristian Skov Johansen <kris701kj@gmail.com>
2022-04-27 20:28:44 +0200

    Replaced the cases in the Census tests

--- Jantimizer/ExperimentSuite/Tests/Census_2013/Cases/higher_capital_loss_in_age_20_to_30_than_30_to_40.sql
+++
@@ -1,8 +0,0 @@
-﻿SELECT C1.id, C1.age, C2.id, C2.age FROM (
-	census as C1 JOIN census as C2 ON
-		C1.age >= 20 AND 
-		C1.age <= 30 AND
-		C2.age > 30 AND 
-		C2.age <= 40 AND
-		C1.capital_loss > C2.capital_loss
-);
--- Jantimizer/ExperimentSuite/Tests/Census_2013/Cases/state_gov_but_higher_capital_gain_than_private.sql
+++
@@ -1,8 +0,0 @@
-﻿SELECT 
-		C1.id, C1.workclass, C1.capital_gain, C2.id, C2.workclass, C2.capital_gain
-	FROM (
-		census as C1 JOIN census as C2 ON 
-			C1.workclass = 'Private' AND 
-			C2.workclass = 'State-gov' AND
-			C2.capital_gain > C1.capital_gain
-	);
