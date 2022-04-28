f652de9005df91ce17f6760003670d54e52c693c
Henrik Ginnerup <hginnerup@hotmail.com>
2022-04-28 09:08:42 +0200

    Added Early closed alpha version of segmentations

---
+++ Jantimizer/Histograms/Models/Segmentations/IHistogramSegmentation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Histograms.Models
+{
+    public class AttributeRef { }
+    public interface IHistogramSegmentation
+    {
+        public IComparable LowestValue { get; set; }
+        /// TODO: How to handle last segment?
+        ///     Just add a last boundary with 1 element?
+
+
+        public long ElementsBeforeNextSegmentation { get; set; }
+    }
+    public interface IHistogramSegmentationComparative : IHistogramSegmentation
+    {
+        public Dictionary<AttributeRef, ulong> CountSmallerThan { get; }
+        public Dictionary<AttributeRef, ulong> CountLargerThan { get; }
+    }
+}
