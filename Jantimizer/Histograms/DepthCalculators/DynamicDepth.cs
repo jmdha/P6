0716777f0fd8d64d4def24d3be058a181df15767
Henrik Ginnerup <hginnerup@hotmail.com>
2022-05-02 00:08:16 +0200

    Added DynamicDepth and tests

---
+++ Jantimizer/Histograms/DepthCalculators/DynamicDepth.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Histograms.DepthCalculators
+{
+    public class DynamicDepth
+    {
+        public DynamicDepth() { }
+
+        public int GetDepth(long uniqueValueCount, long totalValueCount)
+        {
+            var bucketsCount = uniqueValueCount / Math.Log10((uniqueValueCount + 11) / 11);
+            return (int)Math.Ceiling(uniqueValueCount / bucketsCount);
+        }
+    }
+}
