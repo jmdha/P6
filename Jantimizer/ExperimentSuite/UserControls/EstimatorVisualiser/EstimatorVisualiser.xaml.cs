d50cb5a626d7b3acf8d580ee8ced87f1e54ee8b1
Kristian Skov Johansen <kris701kj@gmail.com>
2022-05-09 13:29:47 +0200

    cleanup and returned the experiments file to original state

--- Jantimizer/ExperimentSuite/UserControls/EstimatorVisualiser/EstimatorVisualiser.xaml.cs
+++ Jantimizer/ExperimentSuite/UserControls/EstimatorVisualiser/EstimatorVisualiser.xaml.cs
@@ -14,7 +14,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Tools.Models.JsonModels;
-using System.Linq;
 
 namespace ExperimentSuite.UserControls.EstimatorVisualiser
 {
