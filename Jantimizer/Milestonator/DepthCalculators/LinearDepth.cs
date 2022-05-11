0a4aaa83d02f6b39b7406494f018e5afc4fd492f
Henrik Ginnerup <hginnerup@hotmail.com>
2022-05-11 12:21:26 +0200

    Added LogarithmicCount, LogarithmicDepth and LinearDepth

---
+++ Jantimizer/Milestonator/DepthCalculators/LinearDepth.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milestoner.DepthCalculators
+{
+    public class LinearDepth : BaseDepthCalculator, IDepthCalculator
+    {
+        public LinearDepth(bool shouldUseUniqueValues, double multiplier, int yOffset) : base(shouldUseUniqueValues)
+        {
+            Multiplier = multiplier;
+            YOffset = yOffset;
+        }
+
+        public double Multiplier { get; set; }
+        public int YOffset { get; set; }
+
+        protected override double DepthFunction(long x)
+        {
+            return x * Multiplier + YOffset;
+        }
+    }
+}
